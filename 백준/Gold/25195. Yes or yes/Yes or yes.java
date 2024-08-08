4c12b60e43094fc7c970537b2f1aa25496116811
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-08-08 23:03:00 +0900

    [Gold IV] Title: Yes or yes, Time: 364 ms, Memory: 57248 KB -BaekjoonHub

---
+++ 백준/Gold/25195. Yes or yes/Yes or yes.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Node {
+        int v;
+        boolean flag;
+        Node(int v, boolean flag) {
+            this.v = v;
+            this.flag = flag;
+        }
+    }
+
+    static class Edge {
+        int v;
+        Edge next;
+        Edge(int v, Edge next) {
+            this.v = v;
+            this.next = next;
+        }
+    }
+    static Edge[] graph;
+    static boolean[] fan;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        graph = new Edge[N+1];
+        fan = new boolean[N+1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u] = new Edge(v, graph[u]);
+        }
+
+        int S = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < S; i++) {
+            int s = Integer.parseInt(st.nextToken());
+            fan[s] = true;
+        }
+
+        bfs();
+    }
+
+    private static void bfs() {
+        Queue<Node> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N+1][2];
+
+        if (fan[1]) {
+            visited[1][1] = true;
+            q.offer(new Node(1,true));
+        } else {
+            visited[1][0] = true;
+            q.offer(new Node(1,false));
+        }
+
+        boolean meet = true;
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            int cnt = 0;
+            for (Edge edge = graph[cur.v]; edge != null; edge = edge.next) {
+
+                cnt++;
+                if (cur.flag) {
+                    if (visited[edge.v][1]) continue;
+                    visited[edge.v][1] = true;
+                    q.offer(new Node(edge.v, true));
+                } else { 
+                    if (visited[edge.v][0]) continue;
+
+                    if (!fan[edge.v]) {
+                        visited[edge.v][0] = true;
+                        q.offer(new Node(edge.v, false));
+                    }
+
+                }
+            }
+            if (cnt == 0 && !cur.flag) {
+                meet = false;
+                break;
+            }
+
+        }
+        if (!meet) System.out.println("yes");
+        else System.out.println("Yes");
+
+    }
+}

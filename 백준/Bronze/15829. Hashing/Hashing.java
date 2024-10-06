618b9dbfe7a45d3ee89639aa5651fb86b0e0fe30
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-10-06 14:10:36 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 11696 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,16 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         String s = br.readLine();
-        long H = 0L;
+
+        BigInteger H = new BigInteger("0");
         for (int i = 0; i < L; i++) {
-            H += ((s.charAt(i) - 'a' + 1) * Math.pow(31,i));
+            H = H.add(BigInteger.valueOf(s.charAt(i)-'a'+1).multiply(BigInteger.valueOf(31).pow(i)));
         }
-        System.out.println(H % 1234567891);
+        System.out.println(H.mod(BigInteger.valueOf(1234567891)));
     }
 }

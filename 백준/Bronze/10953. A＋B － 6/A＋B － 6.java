eaa40f4b883150b777bae6a753fabb59866afbb1
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-10-06 13:48:13 +0900

    [Bronze III] Title: A+B - 6, Time: 68 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(),",");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(A+B).append("\n");
+        }
+        System.out.println(sb);
+    }
+}

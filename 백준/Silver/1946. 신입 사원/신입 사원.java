a07de67bc993c1d62d41c79f2e0f04be49c1f52d
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-10-08 19:19:40 +0900

    [Silver I] Title: 신입 사원, Time: 716 ms, Memory: 297464 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -4,39 +4,28 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int index;
-        int s1;
-        int s2;
-        Node(int index, int s1, int s2) {
-            this.index = index;
-            this.s1 = s1;
-            this.s2 = s2;
-        }
-    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-            ArrayList<Node> graph = new ArrayList<>();
+            int[] graph = new int[N+1];
 
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int s1 = Integer.parseInt(st.nextToken());
                 int s2 = Integer.parseInt(st.nextToken());
-                graph.add(new Node(i,s1,s2));
-
+                graph[s1] = s2;
             }
-            Collections.sort(graph, Comparator.comparingInt(o -> o.s2));
-            int standard = graph.get(0).s1;
+            int standard = graph[1];
             int total = 1;
-            for (int j = 1; j < N; j++) {
-                Node cur = graph.get(j);
-                if (cur.s1 < standard) {
+            for (int j = 2; j <= N; j++) {
+                int cur = graph[j];
+                if (cur < standard) {
                     total++;
-                    standard = cur.s1;
+                    standard = cur;
                 }
             }
             sb.append(total).append("\n");

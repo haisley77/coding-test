27b9c443884a52e3eda23c0a007b36aa3a4ca8b7
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-10-08 18:41:15 +0900

    [Silver I] Title: 신입 사원, Time: 1924 ms, Memory: 302356 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static class Node {
+        int index;
+        int s1;
+        int s2;
+        Node(int index, int s1, int s2) {
+            this.index = index;
+            this.s1 = s1;
+            this.s2 = s2;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            ArrayList<Node> graph = new ArrayList<>();
+            int[] check = new int[N];
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int s1 = Integer.parseInt(st.nextToken());
+                int s2 = Integer.parseInt(st.nextToken());
+                graph.add(new Node(i,s1,s2));
+
+            }
+
+            Collections.sort(graph, Comparator.comparingInt(o -> o.s1));
+
+            int standard = graph.get(0).s2;
+            check[graph.get(0).index]++;
+            for (int j = 1; j < N; j++) {
+                Node cur = graph.get(j);
+                if (cur.s2 < standard) {
+                    check[cur.index]++;
+                    standard = cur.s2;
+                }
+
+            }
+
+            Collections.sort(graph, Comparator.comparingInt(o -> o.s2));
+
+            standard = graph.get(0).s1;
+            check[graph.get(0).index]++;
+            for (int j = 1; j < N; j++) {
+                Node cur = graph.get(j);
+                if (cur.s1 < standard) {
+                    check[cur.index]++;
+                    standard = cur.s1;
+                }
+            }
+
+            int total = 0;
+            for (int j = 0; j < N; j++) {
+                if (check[j] == 2) total++;
+            }
+            sb.append(total).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}

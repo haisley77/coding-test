b0109fc24ff43eef98cbf5f6eebf3ae40bbc8621
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-10-09 15:39:01 +0900

    [Gold IV] Title: 트리, Time: 316 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -4,19 +4,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Edge {
-        int a;
-        int b;
-        Edge(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
-    }
+
     static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         for (int t = 1; ; t++){
+            
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
@@ -25,24 +19,18 @@ public class Main {
             parent = new int[N+1];
             for (int i = 1; i <= N; i++) parent[i] = i;
 
-            Edge[] graph = new Edge[M];
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                graph[i] = new Edge(a,b);
-            }
 
-            for (int i = 0; i < M; i++) {
-                Edge cur = graph[i];
-
-                if (find(cur.a) == find(cur.b)) {
-                    union(cur.a, 0);
-                    union(cur.b, 0);
+                if (find(a) == find(b)) {
+                    union(a, 0);
+                    union(b, 0);
                     continue;
                 }
 
-                union(cur.a,cur.b);
+                union(a, b);
             }
 
             boolean[] checked = new boolean[N+1];
@@ -54,7 +42,6 @@ public class Main {
                 checked[set] = true;
                 total++;
             }
-
 
             sb.append("Case ").append(t).append(": ");
             if (total > 1) {
@@ -71,13 +58,12 @@ public class Main {
 
     private static int find(int a) {
         if (parent[a] == a) return a;
-        return parent[a] = find(parent[a]); // 경로 압축
+        return parent[a] = find(parent[a]);
     }
 
     private static void union(int a, int b) {
         a = find(a);
         b = find(b);
-
         if (a < b) parent[b] = a;
         if (a > b) parent[a] = b;
     }

e5cd536dc5e9f54530e6f3bb0be4c627fa7ad80c
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-10-25 15:06:09 +0900

    [level 2] Title: 숫자 변환하기, Time: 104.79 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -5,27 +5,26 @@ class Solution {
 
         
         Queue<Integer> q = new LinkedList<>();
-        int[] visited = new int[1000001];
+        int[] visited = new int[y+1];
         visited[x] = 1;
         q.offer(x);
         
         while (!q.isEmpty()) {
             int cur = q.poll();
             
-            // 목표 지점에 도달하면 최소 연산 횟수 반환
             if (cur == y) {
                 return visited[cur] - 1;
             }
             
-            if (cur + n <= 1000000 && visited[cur + n] == 0){
+            if (cur + n <= y && visited[cur + n] == 0){
                 visited[cur+n] = visited[cur] + 1;
                 q.offer(cur+n);
             }
-            if (cur * 2 <= 1000000 && visited[cur * 2] == 0){
+            if (cur * 2 <= y && visited[cur * 2] == 0){
                 visited[cur*2] = visited[cur] + 1;
                 q.offer(cur*2);
             }
-            if (cur * 3 <= 1000000 && visited[cur * 3] == 0){
+            if (cur * 3 <= y && visited[cur * 3] == 0){
                 visited[cur*3] = visited[cur] + 1;
                 q.offer(cur*3);
             }

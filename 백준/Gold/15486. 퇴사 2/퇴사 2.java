13be1cdfc2f8cccd68e86233cc71d5c01a8ba862
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-10-29 13:14:43 +0900

    [Gold V] Title: 퇴사 2, Time: 624 ms, Memory: 302572 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N+2];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            dp[i+1] = Math.max(dp[i],dp[i+1]);
+
+            if (i+t <= N+1) {       // 종료일
+                dp[i+t] = Math.max(dp[i+t],dp[i]+p);
+            }
+        }
+        System.out.println(dp[N+1]);
+    }
+}

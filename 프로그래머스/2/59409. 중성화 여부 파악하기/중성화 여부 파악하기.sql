8ed06446221316ca6f018d4df8ac33ab5ecfbddb
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-11-01 23:57:52 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,7 +1,5 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME,
-CASE WHEN LOCATE('Spayed', SEX_UPON_INTAKE) >= 1 THEN 'O'
-WHEN LOCATE('Neutered', SEX_UPON_INTAKE) >= 1 THEN 'O'
-ELSE 'X' END AS '중성화'
+-- 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
+SELECT ANIMAL_ID, NAME, CASE WHEN SEX_UPON_INTAKE LIKE 'Spayed%' OR SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+                        ELSE 'X' END AS '중성화'
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID

f574915db2d50a5ab92c22c57cb2797e0ccc9551
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-11-01 23:55:52 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,5 +1,6 @@
--- 코드를 입력하세요
+-- 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
+
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
-WHERE LOCATE('el',NAME) >= 1 AND ANIMAL_TYPE = 'Dog'
+WHERE LOCATE('el', NAME) >= 1 AND ANIMAL_TYPE = 'Dog'
 ORDER BY NAME

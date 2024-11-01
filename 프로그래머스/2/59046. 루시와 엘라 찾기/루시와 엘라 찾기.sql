d83516f048ffa5ec5f25b3cd28f94f40fea40c33
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-11-01 23:51:48 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,6 @@
--- 코드를 입력하세요
+-- 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
+
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
-ORDER BY ANIMAL_ID
+ORDER BY ANIMAL_ID;

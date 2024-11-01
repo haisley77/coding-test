eeb32669a39f4c1bba32024181fe61644d001cec
Haeun Lee <101447960+haisley77@users.noreply.github.com>
2024-11-01 17:47:38 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,15 +1,19 @@
--- 코드를 작성해주세요
-SELECT HE.EMP_NO, HE.EMP_NAME, 
-CASE
-    WHEN AVG(HG.SCORE) >= 96 THEN 'S'
-    WHEN AVG(HG.SCORE) >= 90 THEN 'A'
-    WHEN AVG(HG.SCORE) >= 80 THEN 'B'
-ELSE 'C' END AS GRADE, 
-CASE
-    WHEN AVG(HG.SCORE) >= 96 THEN HE.SAL * 0.2 
-    WHEN AVG(HG.SCORE) >= 90 THEN HE.SAL * 0.15 
-    WHEN AVG(HG.SCORE) >= 80 THEN HE.SAL * 0.1
-ELSE 0 END AS BONUS
-FROM HR_EMPLOYEES HE JOIN HR_GRADE HG ON HE.EMP_NO = HG.EMP_NO
-GROUP BY HE.EMP_NO, HE.EMP_NAME
-ORDER BY EMP_NO
+-- 부서 ID, 국문 부서명, 영문 부서명, 부서 위치
+-- 사번, 성명, 부서 ID, 직책, 이메일, 전화번호, 입사일, 연봉
+-- 사번, 연도, 반기, 평가 점수
+
+WITH T AS (SELECT EMP_NO, 
+                  CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+                       WHEN AVG(SCORE) >= 90 THEN 'A'
+                       WHEN AVG(SCORE) >= 80 THEN 'B'
+                       ELSE 'C' END AS GRADE
+          FROM HR_GRADE
+          GROUP BY EMP_NO)
+          
+SELECT A.EMP_NO, A.EMP_NAME, T.GRADE,
+        CASE WHEN GRADE = 'S' THEN SAL * 0.2
+             WHEN GRADE = 'A' THEN SAL * 0.15
+             WHEN GRADE = 'B' THEN SAL * 0.1
+             ELSE 0 END AS BONUS
+FROM HR_EMPLOYEES A JOIN T ON A.EMP_NO = T.EMP_NO
+ORDER BY A.EMP_NO

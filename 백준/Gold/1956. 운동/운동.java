7b229a9e9e1e7206aace12289750b4c1662cec17
haisley <101447960+haisley77@users.noreply.github.com>
2025-01-12 14:08:56 +0900

    [Gold IV] Title: 운동, Time: 1012 ms, Memory: 73744 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int INF = 1_000_000_000;
+
+        int[][] dist = new int[V + 1][V + 1];
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = c;
+        }
+
+        // Floyd-Warshall Algorithm
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (dist[i][k] < INF && dist[k][j] < INF) { // K 정점 경유해서 i -> j로 갈 수 있다면
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        int res = INF;
+        for (int i = 1; i <= V; i++) {
+            res = Math.min(res, dist[i][i]); // Cycle check
+        }
+
+        if (res == INF) System.out.println(-1);
+        else System.out.println(res);
+    }
+}

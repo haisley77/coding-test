31a972fd8a501079c955a7ce49ffb866da750d94
haisley <101447960+haisley77@users.noreply.github.com>
2025-02-10 12:41:31 +0900

    [Silver II] Title: 사탕 게임, Time: 144 ms, Memory: 11764 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N, res;
+    static char[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+
+        for (int i = 0; i < N ;i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (j < N - 1) {
+                    swapAndFindMax(i,j,i,j+1);
+                }
+
+                if (i < N - 1) {
+                    swapAndFindMax(i,j,i+1,j);
+                }
+            }
+        }
+
+        System.out.println(res);
+
+    }
+
+    private static void swapAndFindMax(int x1, int y1, int x2, int y2) {
+        char temp = arr[x1][y1];
+        arr[x1][y1] = arr[x2][y2];
+        arr[x2][y2] = temp;
+
+        countMaxCandies();
+
+        temp = arr[x1][y1];
+        arr[x1][y1] = arr[x2][y2];
+        arr[x2][y2] = temp;
+    }
+
+    private static void countMaxCandies() {
+
+        for (int i = 0; i < N; i++) {
+            int maxR = 1;
+            int maxC = 1;
+
+            for (int j = 1; j < N; j++) {
+                if (arr[i][j-1] == arr[i][j]) { // i행
+                    maxR++;
+                } else {
+                    res = Integer.max(res, maxR);
+                    maxR = 1;
+                }
+
+                if (arr[j][i] == arr[j-1][i]) { // i열
+                    maxC++;
+                } else {
+                    res = Integer.max(res, maxC);
+                    maxC = 1;
+                }
+            }
+
+            res = Integer.max(res, maxR);
+            res = Integer.max(res, maxC);
+        }
+
+    }
+
+}

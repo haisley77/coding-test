6c91cbebfc33b314ce32af19bf2b8dda7617ec18
haisley <101447960+haisley77@users.noreply.github.com>
2025-02-27 01:09:26 +0900

    [Gold IV] Title: 불, Time: 696 ms, Memory: 120676 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,100 +1,103 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-    private static Queue<int[]> fire;
-    private static Queue<int[]> tmpFire;
-    private static StringBuilder sb = new StringBuilder();
-    private static int w, h;
-
+    static int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+    static Queue<int[]> fq, sq;
+    static boolean[][] visited;
+    static int[][] time;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         for (int t = 0; t < T; t++) {
-            fire = new LinkedList<>();
             StringTokenizer st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
             char[][] map = new char[h][w];
-            int startX = 0;
-            int startY = 0;
-            int cnt = 0;
+
+            fq = new LinkedList<>();
+            sq = new LinkedList<>();
+            visited = new boolean[h][w];    // 불 방문체크
+            time = new int[h][w];
+
             for (int i = 0; i < h; i++) {
                 String s = br.readLine();
                 for (int j = 0; j < w; j++) {
                     map[i][j] = s.charAt(j);
                     if (map[i][j] == '@') {
-                        startX = i;
-                        startY = j;
+                        time[i][j] = 1;
+                        sq.offer(new int[]{i, j});
                     }
                     if (map[i][j] == '*') {
-                        fire.offer(new int[]{i,j});
-                    }
-                    if (i == 0 || i == h-1 || j == 0 || j == w-1) {
-                        if (map[i][j] == '*' || map[i][j] == '#') cnt++;
+                        visited[i][j] = true;
+                        fq.offer(new int[]{i, j});
                     }
                 }
             }
-            if (cnt == (w+h)*2) sb.append("IMPOSSIBLE").append("\n");
-            else {
-                int time = bfs(startX, startY, map);
-                if (time == -1) sb.append("IMPOSSIBLE").append("\n");
-                else sb.append(time).append("\n");
-            }
+
+            int res = bfs(map, w, h);
+            if (res == -1) sb.append("IMPOSSIBLE").append("\n");
+            else sb.append(res).append("\n");
 
         }
         System.out.println(sb);
     }
 
-    private static int bfs(int startX, int startY, char[][] map) {
-        Queue<int[]> q = new LinkedList<>();
-        int[][] visited = new int[h][w];
-        visited[startX][startY] = 1;
-        q.offer(new int[]{startX, startY});
+    private static int bfs(char[][] map, int w, int h) {
 
-        while (!q.isEmpty()) {
-            spreadFire(map); 
+        while (!sq.isEmpty()) {
 
-            int qSize = q.size();
-            for (int k = 0; k < qSize; k++) {
-                int[] cur = q.poll();
+            // 불 이동
+            int qSize = fq.size();
+            for (int k = 0; k < qSize; k++){
+                int[] fire = fq.poll();
 
                 for (int i = 0; i < dirs.length; i++) {
-                    int px = cur[0] + dirs[i][0];
-                    int py = cur[1] + dirs[i][1];
+                    int nx = fire[0] + dirs[i][0];
+                    int ny = fire[1] + dirs[i][1];
+                    if (nx < 0 || nx >= h || ny < 0 || ny >= w || visited[nx][ny]) continue;
 
-                    if (px < 0 || px >= h || py < 0 || py >= w) {
-                        return visited[cur[0]][cur[1]];
+                    if (map[nx][ny] == '.' || map[nx][ny] == '@') {
+                        visited[nx][ny] = true;
+                        map[nx][ny] = '*';
+                        fq.offer(new int[]{nx,ny});
                     }
-                    if (visited[px][py] > 0) continue;
-                    if (map[px][py] == '*' || map[px][py] == '#') continue;
 
-                    visited[px][py] = visited[cur[0]][cur[1]] + 1;
-                    q.offer(new int[]{px, py});
+                }
+
+            }
+
+            // 상근 이동
+            qSize = sq.size();
+            for (int k = 0; k < qSize; k++) {
+                int[] cur = sq.poll();
+
+                for (int i = 0; i < dirs.length; i++) {
+                    int nx = cur[0] + dirs[i][0];
+                    int ny = cur[1] + dirs[i][1];
+
+                    if (nx < 0 || nx >= h || ny < 0 || ny >= w) return time[cur[0]][cur[1]];
+
+                    if (time[nx][ny] > 0) continue;
+
+                    if (map[nx][ny] == '.' || map[nx][ny] == '@') { //
+                        time[nx][ny] = time[cur[0]][cur[1]] + 1;
+                        sq.offer(new int[]{nx,ny});
+                    }
+
                 }
             }
+
         }
 
         return -1;
-    }
-
-    private static void spreadFire(char[][] map) {
-        int size = fire.size();
-
-        for (int i = 0; i < size; i++) {
-            int[] cur = fire.poll();
-            for (int j = 0; j < dirs.length; j++) {
-                int px = cur[0] + dirs[j][0];
-                int py = cur[1] + dirs[j][1];
-                if (px < 0 || px >= h || py < 0 || py >= w) continue;
-                if (map[px][py] == '#' || map[px][py] == '*') continue;
-                map[px][py] = '*';
-                fire.offer(new int[]{px, py});
-            }
-        }
 
     }
+
 }

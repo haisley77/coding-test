8ecba44ad2bbac58e12def14787c4340962f9965
haisley <101447960+haisley77@users.noreply.github.com>
2025-04-07 10:39:47 +0900

    [level 2] Title: 가장 큰 수, Time: 2973.39 ms, Memory: 388 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] res = new String[numbers.length];
+        
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            res[i] = String.valueOf(numbers[i]);
+        }
+        if (sum == 0) return "0";
+        
+        Arrays.sort(res, new Comparator<String>(){
+           @Override
+            public int compare(String n1, String n2) {
+                return (n1+n2).compareTo(n2+n1);
+            }
+        });
+        for (int i = res.length-1; i >= 0; i--) {
+            answer += res[i];
+        }
+        return answer;
+    }
+}

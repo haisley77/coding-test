d10b5fd9832e42dffa95efade1b505a6a25e181f
haisley <101447960+haisley77@users.noreply.github.com>
2025-07-23 22:40:35 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,33 @@
 import java.util.*;
 
 class Solution {
-    static int answer = 0;
-    static boolean[] visited;
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
+        int answer = 0;
         visited = new boolean[n];
         for (int i = 0; i < n; i++) {
             if (visited[i]) continue;
-            bfs(computers, visited, i);
+            bfs(i, computers, n);
             answer++;
         }
         return answer;
     }
-    private void bfs(int[][] computers, boolean[] visited, int s) {
+    
+    private void bfs(int s, int[][] computers, int n) {
         Queue<Integer> q = new LinkedList<>();
         q.offer(s);
         visited[s] = true;
         
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int cur = q.poll();
             
-            for (int i = 0; i < computers.length; i++) {
-                if (visited[i]) continue;   // 이미 방문한 점은 건너뛰기
-                if (computers[cur][i] == 1) {
-                    visited[i] = true;
-                    q.offer(i);
-                }
+            for (int i = 0; i < n; i++) {
+                if (computers[cur][i] == 0) continue;
+                if (visited[i]) continue;
+                
+                visited[i] = true;
+                q.offer(i);
             }
-        }
-        
+        } 
     }
-    
 }

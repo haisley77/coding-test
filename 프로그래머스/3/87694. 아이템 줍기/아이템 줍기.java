d2fb6ca9f283177f1ad83ce31134eccac59225fc
haisley <101447960+haisley77@users.noreply.github.com>
2025-07-24 00:03:43 +0900

    [level 3] Title: 아이템 줍기, Time: 1.58 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -4,58 +4,51 @@ class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
         
-        int[][] map = new int[101][101];    // 50 * 2
-        characterX *= 2;
-        characterY *= 2;
-        itemX *= 2;
-        itemY *= 2;
-        
-        for (int[] cur: rectangle) {
-            cur[0] *= 2;
-            cur[1] *= 2;
-            cur[2] *= 2;
-            cur[3] *= 2;
+        int[][] map = new int[101][101];
+        for (int i = 0; i < rectangle.length; i++) {
+            int[] cur = rectangle[i];
+            for (int j = 0; j < 4; j++) {
+                cur[j] *= 2;
+            }
             
-            for (int i = cur[0]; i <= cur[2]; i++) {
-                for (int j = cur[1]; j <= cur[3]; j++) {
-                    if (i == cur[0] || i == cur[2] || j == cur[1] || j == cur[3]) {
-                        if (map[i][j] == -1) continue;
-                        map[i][j] = 1;
+            for (int j = cur[1]; j <= cur[3]; j++) {
+                for (int k = cur[0]; k <= cur[2]; k++) {
+                    if (j == cur[1] || j == cur[3] || k == cur[0] || k == cur[2]) {
+                        if (map[j][k] != -1) map[j][k] = 1;
                     } else {
-                        map[i][j] = -1;
+                        map[j][k] = -1;
                     }
                 }
             }
+            
         }
         
+        answer = bfs(characterY*2, characterX*2, itemY*2, itemX*2, map);
+        return answer;
+    }
+    
+    private int bfs(int sx, int sy, int ex, int ey, int[][] map) {
         int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-        Queue<int[]> q = new LinkedList<>();
         int[][] visited = new int[101][101];
         
-        q.offer(new int[]{characterX, characterY});
-        visited[characterX][characterY] = 1;
-    
-        ww:
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sx, sy});
+        visited[sx][sy] = 1;
+        
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             
             for (int i = 0; i < dirs.length; i++) {
                 int nx = cur[0] + dirs[i][0];
                 int ny = cur[1] + dirs[i][1];
-                
                 if (nx < 2 || nx > 100 || ny < 2 || ny > 100 || visited[nx][ny] > 0) continue;
-                if (map[nx][ny] != 1) continue; 
-                
-                q.offer(new int[]{nx,ny});
+                if (map[nx][ny] <= 0) continue;
                 visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
-                if (nx ==  itemX && ny == itemY) {
-                    break ww;
-                }
-                
+                q.offer(new int[]{nx,ny});
             }
-        
+            
         }
         
-        return visited[itemX][itemY] / 2;
+        return visited[ex][ey]/2;
     }
 }

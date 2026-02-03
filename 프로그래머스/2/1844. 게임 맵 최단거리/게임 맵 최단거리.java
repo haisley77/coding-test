6c13b89dd963dad359131c0080c4ab9ca47bc9e3
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-03 14:13:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.92 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,39 @@
 import java.util.*;
 
 class Solution {
+    int n, m;
     public int solution(int[][] maps) {
-        return bfs(maps, maps.length, maps[0].length);
+        n = maps.length;
+        m = maps[0].length;
+        int answer = bfs(maps);
+        return answer;
     }
-    private int bfs(int[][] maps, int h, int w) {
-        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
     
-        int[][] visited = new int[h][w];
+    private int bfs(int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        int[][] visited = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visited[i], 1);
+        }
         q.offer(new int[]{0,0});
-        visited[0][0] = 1;
         
         while (!q.isEmpty()) {
             int[] cur = q.poll();
+            if (cur[0] == n && cur[1] == m) break;
             
             for (int i = 0; i < dirs.length; i++) {
                 int nx = cur[0] + dirs[i][0];
                 int ny = cur[1] + dirs[i][1];
                 
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w || visited[nx][ny] > 1) continue;
-                if (maps[nx][ny] == 0) continue;
-                
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] > 1 || maps[nx][ny] == 0) continue;
+            
                 visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
-                q.offer(new int[]{nx,ny});
+                q.offer(new int[]{nx, ny});
             }
         }
         
-        if (visited[h-1][w-1] == 0) return -1;
-        else return visited[h-1][w-1];
+        if (visited[n-1][m-1] == 1) return -1;
+        return visited[n-1][m-1];
     }
 }

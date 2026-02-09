ede806564cdc7aba8fbe0188b4930db6ba5374c1
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-09 20:33:23 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,16 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int si = 0;
+        
         for (int i = 0; i < commands.length; i++) {
-            int[] newArr = new int[commands[i][1] - commands[i][0] + 1];
-            int index = 0;
-            for (int j = commands[i][0]-1; j <= commands[i][1]-1; j++) {
-                newArr[index++] = array[j];
+            int[] cur = commands[i];
+            
+            int[] arr = new int[cur[1] - cur[0] + 1];
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = array[cur[0] - 1 + j];
             }
-            Arrays.sort(newArr);
-            answer[si++] = newArr[commands[i][2]-1];
+            Arrays.sort(arr);
+            answer[i] = arr[cur[2] - 1];
+            
         }
+        
         return answer;
     }
 }

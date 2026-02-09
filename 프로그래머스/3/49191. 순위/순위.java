2f9cfb68572d061d7d9ae26ce7e039602ac0741c
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-09 21:54:20 +0900

    [level 3] Title: 순위, Time: 7.52 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -11,44 +11,40 @@ class Solution {
     }
     public int solution(int n, int[][] results) {
         int answer = 0;
-        Node[] graph1 = new Node[n+1];  // win
-        Node[] graph2 = new Node[n+1]; 
+        Node[] graph1 = new Node[n+1];
+        Node[] graph2 = new Node[n+1];
         for (int i = 0; i < results.length; i++) {
-            int a = results[i][0];
-            int b = results[i][1];
-            // a가 b를 이김
-            graph1[a] = new Node(b,graph1[a]);
-            graph2[b] = new Node(a,graph2[b]);
+            graph1[results[i][0]] = new Node(results[i][1], graph1[results[i][0]]); 
+            graph2[results[i][1]] = new Node(results[i][0], graph2[results[i][1]]);
         }
-        
-        for (int i = 1; i <= n; i++) {
-            int cnt1 = bfs(i,n,graph1); // 이긴 사람 수
-            int cnt2 = bfs(i,n,graph2); // 진 사람 수
-            if (cnt1+cnt2 == n-1) answer++;
+         
+        for (int i = 1; i < n+1; i++) {
+            int tmp = 0;
+            tmp += bfs(graph1, n, i);
+            tmp += bfs(graph2, n, i);
+            if (tmp == n-1) answer++;
         }
         
         return answer;
     }
     
-    int bfs(int s, int n, Node[] graph) {
-        int cnt = 0;
-        
+    private int bfs(Node[] graph, int n, int s) {
         Queue<Integer> q = new LinkedList<>();
         boolean[] visited = new boolean[n+1];
         visited[s] = true;
         q.offer(s);
         
+        int cnt = 0;
         while (!q.isEmpty()) {
             int cur = q.poll();
             cnt++;
+            
             for (Node node = graph[cur]; node != null; node = node.next) {
                 if (visited[node.v]) continue;
                 visited[node.v] = true;
                 q.offer(node.v);
             }
         }
-        
-        return cnt - 1;
+        return cnt-1;
     }
-
 }

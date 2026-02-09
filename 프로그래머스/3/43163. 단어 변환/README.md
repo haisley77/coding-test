6625a626f76af45eb44f1aa4c38db5fafb7ac586
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-09 16:38:50 +0900

    [level 3] Title: 단어 변환, Time: 0.33 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 0.61 ms
+메모리: 81.8 MB, 시간: 0.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 28일 14:21:11
+2026년 02월 09일 16:38:47
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,52 +1,44 @@
 import java.util.*;
-class Node {
-    String now;
-    int att;
-    Node(String now, int att) {
-        this.now = now;
-        this.att = att;
-    }
-}
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 1;
-        
-        if (begin.length() != target.length()) return 0;
-        if (begin.length() != words[0].length()) return 0;
-        
-        boolean flag = false;
-        for (int i = 0; i < words.length; i++) {
-            if (target.equals(words[i])) flag = true;
-        }
-        if (!flag) return 0;
-        
+        Queue<int[]> q = new LinkedList<>();
         boolean[] visited = new boolean[words.length];
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(begin,0));
         
+        // 처음에 변환 가능한 문자열 찾기
+        for (int i = 0; i < words.length; i++) {
+            if (check(begin, words[i]) == 1) {
+                visited[i] = true;
+                q.offer(new int[]{i, 1});
+            }
+        }
+        
+        int answer = 0;
         while (!q.isEmpty()) {
-            Node cur = q.poll();
-            String now = cur.now;
-            if (now.equals(target)) return cur.att;
-            
-            for (int i = 0; i < words.length; i++) {
-                
-                if (visited[i]) continue;
-                
-                int cnt = 0;
-                for (int j = 0; j < now.length(); j++) {
-                    if (now.charAt(j) == words[i].charAt(j)) cnt++;
-                }
-                
-                if (cnt == now.length() - 1) {
-                    visited[i] = true;
-                    q.offer(new Node(words[i], cur.att+1));
-                }
-                
+            int[] cur = q.poll();
+            if (words[cur[0]].equals(target)) {
+                answer = cur[1];
+                break;
             }
             
+            for (int i = 0; i < words.length; i++) {
+                if (visited[i]) continue;
+                if (check(words[cur[0]], words[i]) == 1) {
+                    visited[i] = true;
+                    q.offer(new int[]{i, cur[1] + 1});
+                }
+            }
         }
         
         return answer;
+    }
+    
+    private int check(String s1, String s2) {
+        int cnt = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) cnt++;            
+        }
+        if (cnt == s1.length() - 1) return 1; // s1, s2 하나만 다르면
+        return 0; // 나머지
     }
 }

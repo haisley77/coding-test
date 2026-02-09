67313cb2889dad476b6eb87c6ffe0f2e7ce5ae70
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-09 20:39:28 +0900

    [level 2] Title: 가장 큰 수, Time: 2840.20 ms, Memory: 393 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -11,13 +11,12 @@ class Solution {
             res[i] = String.valueOf(numbers[i]);
         }
         if (sum == 0) return "0";
-        
-        Arrays.sort(res, new Comparator<String>(){
-           @Override
+        Arrays.sort(res, new Comparator<String>() {
+            @Override
             public int compare(String n1, String n2) {
                 return (n1+n2).compareTo(n2+n1);
             }
-        });
+        }); // 오름차순정렬
         for (int i = res.length-1; i >= 0; i--) {
             answer += res[i];
         }

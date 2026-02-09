f4a4a07f6c30484be773fdee8e45801d7951e348
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-09 21:30:20 +0900

    [level 2] Title: H-Index, Time: 1.52 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,17 +1,17 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
-        int n = citations.length;
-        int hIndex = 0;
         Arrays.sort(citations);
-        for (int i = 0; i < n; i++){
-            int h = n - i;
-            if (citations[i] >= h) {
-                hIndex = h;
+        int n = citations.length;
+    
+        int h = 0;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                h = n - i;
                 break;
             }
         }
-        return hIndex;
+        
+        return h;
     }
 }

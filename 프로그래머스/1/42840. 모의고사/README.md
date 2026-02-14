a502a2c674db67a0e7476e39a4b5541738fffcf3
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-14 13:53:59 +0900

    [level 1] Title: 모의고사, Time: 0.94 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 2.46 ms
+메모리: 80.3 MB, 시간: 0.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 09일 21:25:12
+2026년 02월 14일 13:53:56
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,52 +1,53 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-
-        int max = 0;
-        int[] S = new int[3];
-        int tr = 0;
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == tr+1) S[0]++;
-            tr = (tr + 1) % 5;
-        }
-        if (max < S[0]) max = S[0];
-        tr = 0;
-        for (int i = 0; i < answers.length; i++) {
-            if (tr == 0 && answers[i] == 2) S[1]++;
-            if (tr == 1 && answers[i] == 1) S[1]++;
-            if (tr == 2 && answers[i] == 2) S[1]++;
-            if (tr == 3 && answers[i] == 3) S[1]++;
-            if (tr == 4 && answers[i] == 2) S[1]++;
-            if (tr == 5 && answers[i] == 4) S[1]++;
-            if (tr == 6 && answers[i] == 2) S[1]++;
-            if (tr == 7 && answers[i] == 5) S[1]++;
-            tr = (tr + 1) % 8;
-        }
-        if (max < S[1]) max = S[1];
-        tr = 0;
-        for (int i = 0; i < answers.length; i++) {
-            if (tr == 0 && answers[i] == 3) S[2]++;
-            if (tr == 1 && answers[i] == 3) S[2]++;
-            if (tr == 2 && answers[i] == 1) S[2]++;
-            if (tr == 3 && answers[i] == 1) S[2]++;
-            if (tr == 4 && answers[i] == 2) S[2]++;
-            if (tr == 5 && answers[i] == 2) S[2]++;
-            if (tr == 6 && answers[i] == 4) S[2]++;
-            if (tr == 7 && answers[i] == 4) S[2]++;
-            if (tr == 8 && answers[i] == 5) S[2]++;
-            if (tr == 9 && answers[i] == 5) S[2]++;
-            tr = (tr + 1) % 10;
-        }
-        if (max < S[2]) max = S[2];
         
-        int cnt = 0;
-        for (int i = 0; i < 3; i++) {
-            if (max == S[i]) cnt++;
+        int[] arr1 = new int[answers.length];
+        int[] arr2 = new int[answers.length];
+        int[] arr3 = new int[answers.length];
+        
+        int[] temp2 = {1,3,4,5};
+        int[] temp3 = {3,3,1,1,2,2,4,4,5,5};
+        
+        int index = 0;
+        
+        for (int i = 0; i < answers.length; i++) {
+            
+            // 수포자1
+            arr1[i] = (i % 5) + 1;
+            
+            // 수포자2
+            if (i % 2 == 0) {
+                arr2[i] = 2;
+            } else {
+                arr2[i] = temp2[index];
+                index = (index + 1) % 4;
+            }
+            
+            // 수포자3
+            arr3[i] = temp3[i % 10];
         }
         
-        int[] answer = new int[cnt];
-        int idx = 0;
-        for (int i = 0; i < 3; i++) {
-            if (max == S[i]) answer[idx++] = i+1; 
+        int sum1 = 0, sum2 = 0, sum3 = 0;
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (arr1[i] == answers[i]) sum1++;
+            if (arr2[i] == answers[i]) sum2++;
+            if (arr3[i] == answers[i]) sum3++;
+        }
+        
+        int max = Math.max(sum1, Math.max(sum2, sum3));
+        
+        List<Integer> list = new ArrayList<>();
+        
+        if (sum1 == max) list.add(1);
+        if (sum2 == max) list.add(2);
+        if (sum3 == max) list.add(3);
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
         
         return answer;

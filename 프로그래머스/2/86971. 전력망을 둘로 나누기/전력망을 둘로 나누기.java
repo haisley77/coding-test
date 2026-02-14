4faef8abfd17f54c37cbb34dd8d16c68db23c6e3
haisley <101447960+haisley77@users.noreply.github.com>
2026-02-14 15:43:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.18 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,56 +1,46 @@
 import java.util.*;
 
 class Solution {
-    class Edge {
+    class Node{
         int v;
-        Edge next;
-        Edge(int v, Edge next) {
+        Node next;
+        Node(int v, Node next) {
             this.v = v;
             this.next = next;
-        } 
+        }
     }
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        Edge[] graph = new Edge[n+1];
-        for (int i = 0; i < wires.length; i++) {
-            int[] cur = wires[i];
-            graph[cur[0]] = new Edge(cur[1],graph[cur[0]]);
-            graph[cur[1]] = new Edge(cur[0],graph[cur[1]]);
+        int answer = n+1;
+        Node[] graph = new Node[n+1];
+        for (int i = 0; i < n-1; i++) {
+            graph[wires[i][0]] = new Node(wires[i][1], graph[wires[i][0]]);
+            graph[wires[i][1]] = new Node(wires[i][0], graph[wires[i][1]]);
         }
-        
-        for (int i = 0; i < wires.length; i++) {
-            int cnt1 = bfs(n, wires[i][0], wires[i][1], graph);
-            
-            int cnt2 = n - cnt1;
-            
-            if (Math.abs(cnt1 - cnt2) < answer) answer = Math.abs(cnt1 - cnt2);
+        for (int i = 0; i < n-1; i++) {
+            int a = bfs(wires[i][0], wires[i][1], n, graph);
+            int b = bfs(wires[i][1], wires[i][0], n, graph);
+            if (Math.abs(a-b) < answer) answer = Math.abs(a-b);
         }
-        
-        
         return answer;
     }
     
-    private int bfs(int n, int v1, int v2, Edge[] graph) {
+    public int bfs(int s, int x, int n, Node[] graph) {
         Queue<Integer> q = new LinkedList<>();
         boolean[] visited = new boolean[n+1];
-        q.offer(1);
-        visited[1] = true;
+        visited[s] = true;
+        q.offer(s);
         
-        int cnt = 0;
+        int num = 0;
         while (!q.isEmpty()) {
             int cur = q.poll();
-            cnt++;
-            for (Edge edge = graph[cur]; edge != null; edge = edge.next) {
-                if (cur == v1 && edge.v == v2) continue;
-                if (cur == v2 && edge.v == v1) continue;
-                
-                if (visited[edge.v]) continue;
-                visited[edge.v] = true;
-                q.offer(edge.v);
+            num++; 
+            for (Node node = graph[cur]; node != null; node = node.next) {
+                if (visited[node.v]) continue;
+                if (node.v == x) continue;
+                visited[node.v] = true;
+                q.offer(node.v);
             }
         }
-        
-        return cnt;
-        
+        return num;
     }
 }
